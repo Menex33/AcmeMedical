75eac3ad671bf860bc24b64cbf45ec35528b246a
Carlos Meneses <cmeneses5@unab.edu.co>
2020-11-02 21:48:43 -0500

    Loging con persistencia local

---
+++ app/src/main/java/co/edu/unab/acmemedical/MainActivity.java
@@ -0,0 +1,22 @@
+package co.edu.unab.acmemedical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvNombreUsuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvNombreUsuario = findViewById(R.id.tv_nombre_usuario);
+        String nombre = getIntent().getStringExtra("nombre");
+        tvNombreUsuario.setText(nombre);
+
+    }
+}
